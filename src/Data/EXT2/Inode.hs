fdd8d3a670e3b33b0595e229f49a779623d1d58f
Braden Walters <vc@braden-walters.info>
2015-04-07 10:51:23 -0400

    Inode modes as data type

--- src/Data/EXT2/Inode.hs
+++ src/Data/EXT2/Inode.hs
@@ -11,6 +11,8 @@ module Data.EXT2.Inode
 import Control.Applicative
 import Control.Monad
 import Data.Binary.Get
+import Data.Bits
+import qualified Data.Bits.Bitwise as Bitwise
 import qualified Data.ByteString as SBS
 import qualified Data.ByteString.Lazy as LBS
 import Data.EXT2.BlockGroupDescriptor
@@ -19,9 +21,27 @@ import Data.EXT2.Util (createTime)
 import Data.UnixTime
 import System.IO
 
+data InodeMode = SocketInode | SymLinkInode | RegFileInode | BlockDevInode |
+                 DirectoryInode | CharDevInode | FifoInode deriving (Show)
+
+inodeModes :: [(Integer, InodeMode)]
+inodeModes = [
+    (0xc000, SocketInode),
+    (0xa000, SymLinkInode),
+    (0x8000, RegFileInode),
+    (0x6000, BlockDevInode),
+    (0x4000, DirectoryInode),
+    (0x2000, CharDevInode),
+    (0x1000, FifoInode)
+  ]
+
+intToInodeModes :: Integer -> [InodeMode]
+intToInodeModes integer =
+  map snd $ filter (\(code, _) -> Bitwise.or (code .&.  integer)) inodeModes
+
 data Inode =
   Inode
-  { mode :: Integer
+  { mode :: [InodeMode]
   , userId :: Integer
   , size :: Integer
   , accessTime :: UnixTime
@@ -50,11 +70,11 @@ fetchInodeTable sb bgd handle = do
 getInodeTable :: Integer -> Get [Inode]
 getInodeTable 0 = return []
 getInodeTable remaining = do
-  inode <- Inode <$> getShort <*> getShort <*> getInt <*> getTime <*> getTime
-                 <*> getTime <*> getTime <*> getShort <*> getShort <*> getShort
-                 <*> getInt <*> getByteString 4 <*> replicateM 15 getInt
-                 <*> getInt <*> getInt <*> getInt <*> getInt
-                 <*> getByteString 12
+  inode <- Inode <$> intToInodeModes <$> (fromIntegral <$> getWord16le)
+                 <*> getShort <*> getInt <*> getTime <*> getTime <*> getTime
+                 <*> getTime <*> getShort <*> getShort <*> getShort <*> getInt
+                 <*> getByteString 4 <*> replicateM 15 getInt <*> getInt
+                 <*> getInt <*> getInt <*> getInt <*> getByteString 12
   (inode:) <$> (getInodeTable (remaining - 1))
   where getInt = toInteger <$> getWord32le
         getShort = toInteger <$> getWord16le
