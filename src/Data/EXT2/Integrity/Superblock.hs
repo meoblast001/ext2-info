ed08cfa42e61ad0df059716338848ef6a532deff
Ricky Elrod <ricky@elrod.me>
2015-04-18 21:34:48 -0400

    Add a function for superblock consistency checking
    @meoblast001 will hate my use of ViewPatterns here.
    
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Data/EXT2/Integrity/Superblock.hs
+++ src/Data/EXT2/Integrity/Superblock.hs
@@ -14,12 +14,20 @@
 -- Functions for checking the integrity of 'Superblock's.
 module Data.EXT2.Integrity.Superblock
 ( superblockMagicCheck
+, superblockConsistency
 ) where
 
 import Data.EXT2.Info.Types
 import Data.EXT2.Superblock
 
--- | Given a superblock ensure that its magic number is as-expected.
+-- | Given a 'Superblock', ensure that its magic number is as-expected.
 superblockMagicCheck :: Superblock -> IntegrityStatus EXT2Error
 superblockMagicCheck (checkIdent -> Left e) = Inconsistent e
 superblockMagicCheck _ = Consistent
+
+-- | Ensure consistency of two 'Superblock's.
+--
+-- This is given for free by 'Superblock'\'s 'Eq' instance.
+superblockConsistency :: Superblock -> Superblock -> IntegrityStatus EXT2Error
+superblockConsistency x ((== x) -> True) = Consistent
+superblockConsistency _ _ = Inconsistent InconsistentSuperblocks
